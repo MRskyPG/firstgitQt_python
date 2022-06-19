0acd11c80ebed5c7341c81b2aa579b48172bd7a1
Рогальский Михаил Владиславович <m.rogalskij.2021@stud.nstu.ru>
2022-06-19 20:38:31 +0700

    Start program from main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # pip install requirements.txt
+import qt_program_1 as qt1
+import sys
+
+
+app = qt1.QtWidgets.QApplication(sys.argv)
+MainWindow = qt1.QtWidgets.QMainWindow()
+ui = qt1.Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+
+
+# def on_click():
+#     print("click!")
+
+# ui.name_of_Button.clicked.connect(name_of_function)
+
+
+sys.exit(app.exec())
--- qt_program_1.py
+++ qt_program_1.py
@@ -44,11 +44,4 @@ class Ui_MainWindow(object):
         self.pushButton_3.setText(_translate("MainWindow", "PushButton"))
 
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec())
+
